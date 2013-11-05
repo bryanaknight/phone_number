9bf5d207df1bb4bedb2af30b63441e0dcbde4c5c
Bryana Knight <brknig11@gmail.com>
2013-11-05 09:13:15 -0700

    refactor

--- phone_number.rb
+++ phone_number.rb
@@ -2,20 +2,16 @@ class PhoneNumber
   attr_reader :phone_number
 
   def initialize(phone_number)
-    @phone_number = phone_number
+    @phone_number = phone_number.gsub(/\D/, '')
   end
 
   def number
-    if phone_number.length == 11 && phone_number[0] == "1"
-      phone_number[1..-1]
-    elsif phone_number.length == 12 && phone_number[0] == "1" && !phone_number.match(/\W/)
-      "0000000000"
-    elsif phone_number.length == 11
-      "0000000000"
-    elsif phone_number.length == 9
+    if valid_number_with_initial_one?
+      remove_initial_one
+    elsif invalid?
       "0000000000"
     else
-      phone_number.gsub(/\D/, '')
+      phone_number
     end
   end
 
@@ -24,18 +20,34 @@ class PhoneNumber
   end
 
   def to_s
-    if phone_number.length == 11
-      new_num = phone_number[1..-1]
-      format_phone_number(new_num)
+    if valid_number_with_initial_one?
+      new_num = remove_initial_one
+      format_phone_number(remove_initial_one)
     else
       format_phone_number(phone_number)
     end 
   end
 
+  private
+
+  def valid_number?
+    phone_number.length == 10 
+  end
+
+  def remove_initial_one
+    phone_number[1..-1]
+  end
+
+  def valid_number_with_initial_one?
+    phone_number.length == 11 && phone_number[0] == "1"
+  end
+
+  def invalid?
+    !valid_number? && !valid_number_with_initial_one?
+  end
+
   def format_phone_number(number)
-    "(" + "#{number[0..2]}" + ") " + 
-    "#{number[3..5]}" + "-" + 
-    "#{number[6..-1]}" 
+    "(#{number[0..2]}) #{number[3..5]}-#{number[6..-1]}" 
   end
 
 end
